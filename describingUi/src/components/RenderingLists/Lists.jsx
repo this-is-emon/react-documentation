61a54ee294f659082153ceebea0301f0db3f7bcc
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-28 19:40:25 +0600

    Restructuring data into a different file

--- describingUi/src/components/RenderingLists/Lists.jsx
+++ describingUi/src/components/RenderingLists/Lists.jsx
@@ -2,44 +2,8 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 
+import { people } from "./data";
 import { getImageUrl } from "./utils";
-
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-    accomplishment: "spaceflight calculations",
-    imageId: "MK3eW3A",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-    accomplishment: "discovery of Arctic ozone hole",
-    imageId: "mynHUSa",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-    accomplishment: "electromagnetism theory",
-    imageId: "bE7W1ji",
-  },
-  {
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-    accomplishment:
-      "pioneering cortisone drugs, steroids and birth control pills",
-    imageId: "IOjWm71",
-  },
-  {
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-    accomplishment: "white dwarf star mass calculations",
-    imageId: "lrWQx8l",
-  },
-];
 
 function Lists() {
   //This method takes an array of items, passes them through a “test” (a function that returns true or false),
